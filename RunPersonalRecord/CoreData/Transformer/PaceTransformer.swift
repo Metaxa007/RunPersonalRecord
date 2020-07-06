8ab1b1195b3883d79bb57c8a777b094b01b5d650
Artsem Lemiasheuski <alemiasheuski@esri.com>
2020-07-06 22:50:53 +0200

    Add comments.

--- RunPersonalRecord/CoreData/Transformer/PaceTransformer.swift
+++ RunPersonalRecord/CoreData/Transformer/PaceTransformer.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2020 metaxa.RunPersonalRecord. All rights reserved.
 //
 
+//https://www.kairadiagne.com/2020/01/13/nssecurecoding-and-transformable-properties-in-core-data.html One or more models in this application are using transformable properties with transformer names that are either unset, or set to NSKeyedUnarchiveFromDataTransformerName.
+
 import Foundation
 
 // 1. Subclass from `NSSecureUnarchiveFromDataTransformer`
