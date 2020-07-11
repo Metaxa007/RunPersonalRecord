1cb9f9d7c8b291e44db32774d64220df8d1204a2
Artsem Lemiasheuski <alemiasheuski@esri.com>
2020-07-11 23:27:27 +0200

    Add RecordsCell, Navigation Controller.

---
+++ RunPersonalRecord/Cell/RecordsCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  RecordsCollectionViewCell.swift
+//  RunPersonalRecord
+//
+//  Created by Artsem Lemiasheuski on 11.07.20.
+//  Copyright © 2020 metaxa.RunPersonalRecord. All rights reserved.
+//
+
+import UIKit
+
+class RecordsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var distanceTextView: UILabel!
+    
+}
