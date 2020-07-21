fc570f69d9b1ebff4f6e9a0165811cc7e68eccdc
Artsem Lemiasheuski <alemiasheuski@esri.com>
2020-07-21 23:08:13 +0200

    Add new run VC.

---
+++ RunPersonalRecord/ViewController/AddRunViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddRunViewController.swift
+//  RunPersonalRecord
+//
+//  Created by Artsem Lemiasheuski on 21.07.20.
+//  Copyright © 2020 metaxa.RunPersonalRecord. All rights reserved.
+//
+
+import UIKit
+
+class AddRunViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+    }
+
+}
