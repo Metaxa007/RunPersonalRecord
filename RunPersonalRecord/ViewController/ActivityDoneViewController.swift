0f9c8607422ab8e59ee8713b92e651e7d715bbb9
Artsem Lemiasheuski <alemiasheuski@esri.com>
2020-09-25 21:33:45 +0200

    Add ActivityDoneViewController.

---
+++ RunPersonalRecord/ViewController/ActivityDoneViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ActivityDoneViewController.swift
+//  RunPersonalRecord
+//
+//  Created by Artsem Lemiasheuski on 26.09.20.
+//  Copyright © 2020 metaxa.RunPersonalRecord. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ActivityDoneViewController: UIViewController {
+
+    @IBOutlet weak var doneLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var infoTableView: UITableView!
+    @IBOutlet weak var splitsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
