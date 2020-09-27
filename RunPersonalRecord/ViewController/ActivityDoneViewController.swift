d5d7e885b65ef41b4391000fcdbb0ddd3043df80
Artsem Lemiasheuski <alemiasheuski@esri.com>
2020-09-27 22:29:14 +0200

    Get activity.

--- RunPersonalRecord/ViewController/ActivityDoneViewController.swift
+++ RunPersonalRecord/ViewController/ActivityDoneViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 import MapKit
 
 class ActivityDoneViewController: UIViewController {
-
+    
     @IBOutlet weak var doneLabel: UILabel!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var infoTableView: UITableView!
     @IBOutlet weak var splitsTableView: UITableView!
     
+    private var paceDic = [Int : Double]()
+    private var restDistPaceDic = [Int : Double]()
+    
+    private var activity: ActivityEntity!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        activity = getActivity()
+        
+        paceDic = activity.pace?.getPace() ?? [:]
+        restDistPaceDic = activity.pace?.getRestDistance() ?? [:]
     }
     
+    private func getActivity() -> ActivityEntity? {
+        let activites = CoreDataManager.manager.getAllEntities()
 
+        //Crash if last activity does not exist for some reason
+        return activites![activites!.count - 1]
+    }
+    
+}
+
+extension ActivityDoneViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView == infoTableView {
+            return 4
+        } else if tableView == splitsTableView {
+            return paceDic.count + restDistPaceDic.count
+        }
+        
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+    
+    
 }
