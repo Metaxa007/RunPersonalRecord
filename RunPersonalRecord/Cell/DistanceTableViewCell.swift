bb4f35b619181c6cf2ce4726f886a4b781791b9f
Artsem Lemiasheuski <alemiasheuski@esri.com>
2020-11-14 21:38:56 +0100

    Constraints for the cell. Enum for distances.

--- RunPersonalRecord/Cell/DistanceTableViewCell.swift
+++ RunPersonalRecord/Cell/DistanceTableViewCell.swift
@@ -9,16 +9,61 @@
 import UIKit
 
 class DistanceTableViewCell: UITableViewCell {
+    
+    private let distanceLabel = UILabel()
+    private let nameLabel = UILabel()
+    private let durationLabel = UILabel()
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
+        setupConstraints()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    private func setupConstraints() {
+        let stackView = UIStackView()
+        stackView.axis = NSLayoutConstraint.Axis.vertical
+        stackView.alignment = UIStackView.Alignment.trailing
+        stackView.spacing = 5
+        stackView.addArrangedSubview(nameLabel)
+        stackView.addArrangedSubview(durationLabel)
+        
+        nameLabel.font = UIFont.systemFont(ofSize: 19, weight: .light)
+        nameLabel.adjustsFontSizeToFitWidth = true
+        nameLabel.minimumScaleFactor = 0.5
+        nameLabel.font = UIFont.systemFont(ofSize: 19, weight: .light)
+        distanceLabel.adjustsFontSizeToFitWidth = true
+        distanceLabel.minimumScaleFactor = 0.5
+        nameLabel.font = UIFont.systemFont(ofSize: 19, weight: .light)
+        durationLabel.adjustsFontSizeToFitWidth = true
+        durationLabel.minimumScaleFactor = 0.5
+        
+        distanceLabel.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.addSubview(distanceLabel)
+        contentView.addSubview(stackView)
+        
+        distanceLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        distanceLabel.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor, constant: 5).isActive = true
+        
+        stackView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor).isActive = true
+    }
+    
+    func setupCell(distance: Distance, name: String, duration: String) {
+        switch distance{
+        case .Marathon:
+            distanceLabel.text = Utilities.manager.getDistanceInKmAsString(distance: distance.rawValue) + " (Marathon)"
+        case .Halfmarathon:
+            distanceLabel.text = Utilities.manager.getDistanceInKmAsString(distance: distance.rawValue) + " (Halfmarathon)"
+        case .Mile:
+            distanceLabel.text = Utilities.manager.getDistanceInKmAsString(distance: distance.rawValue) + " (Mile)"
+        default:
+            distanceLabel.text = Utilities.manager.getDistanceInKmAsString(distance: distance.rawValue)
+        }
+        nameLabel.text = name
+        durationLabel.text = duration
     }
 
 }
--- RunPersonalRecord/ViewController/DistanceListViewController.swift
+++ RunPersonalRecord/ViewController/DistanceListViewController.swift
@@ -16,22 +16,23 @@ class DistanceListViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     private var distance:Int?
-    private let records = [(42195, "Eliud Kipchoge", "2:01:39"),
-                           (30000, "Moses Mosop", "1:26:47"),
-                           (25000, "Moses Mosop", "1:12:25"),
-                           (21097, "Geoffrey Kamworor", "58:01"),
-                           (20000, "Haile Gebrselassie", "56:25"),
-                           (15000, "Joshua Cheptegei", "41:05"),
-                           (10000, "Joshua Cheptegei", "26:11"),
-                           (5000, "Joshua Cheptegei", "12.35"),
-                           (3000, "Daniel Komen", "7:20"),
-                           (2000, "Hicham El Guerrouj", "4:44"),
-                           (1609, "Hicham El Guerrouj", "3:43"),
-                           (1000, "Hicham El Guerrouj", "2:11"),
-                           (800, "David Rudisha", "1:40"),
-                           (400, "Wayde van Niekerk", "43,03s"),
-                           (200, "Usain Bolt", "19,19s"),
-                           (100, "Usain Bolt", "9.58s")]
+    private let records = [(distance: Distance.Marathon, name: "Eliud Kipchoge", duration: "2h:01m:39s", date: "16.09.2018"),
+                           (distance: Distance.KM30, name: "Moses Mosop", duration: "1h:26m:47s", date: "03.06.2011"),
+                           (distance: Distance.KM25, name: "Moses Mosop", duration: "1h:12n:25s", date: "03.06.2011"),
+                           (distance: Distance.Halfmarathon, name: "Geoffrey Kamworor", duration: "58m:01s", date: "15.09.2019"),
+                           (distance: Distance.KM20, name: "Haile Gebrselassie", duration: "56m:25s", date: "27.06.2007"),
+                           (distance: Distance.KM15, name: "Joshua Cheptegei", duration: "41m:05s", date: "18.11.2018"),
+                           (distance: Distance.KM10, name: "Joshua Cheptegei", duration: "26m:11s", date: "07.10.2020"),
+                           (distance: Distance.KM5, name: "Joshua Cheptegei", duration: "12m.35s", date: "14.08.2020"),
+                           (distance: Distance.KM3, name: "Daniel Komen", duration: "7m:2s0", date: "01.09.1996"),
+                           (distance: Distance.KM2, name: "Hicham El Guerrouj", duration: "4m:44s", date: "07.09.1999"),
+                           (distance: Distance.Mile, name: "Hicham El Guerrouj", duration: "3m:43s", date: "07.07.1999"),
+                           (distance: Distance.OneAndHalf, name: "Hicham El Guerrouj", duration: "3m:26s", date: "14.07.1998"),
+                           (distance: Distance.KM1, name: "Noah Ngeny", duration: "2m:11s", date: "05.09.1999"),
+                           (distance: Distance.M800, name: "David Rudisha", duration: "1m:40s", date: "09.08.2012"),
+                           (distance: Distance.M400, name: "Wayde van Niekerk", duration: "43.03s", date: "14.08.2016"),
+                           (distance: Distance.M200, name: "Usain Bolt", duration: "19.19s", date: "20.08.2009"),
+                           (distance: Distance.M100, name: "Usain Bolt", duration: "9.58s", date: "16.08.2009")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,7 +58,7 @@ extension DistanceListViewController: UITableViewDelegate, UITableViewDataSource
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: distanceCell) as? DistanceTableViewCell {
-            cell.textLabel?.text = "\(records[indexPath.row].0)"
+            cell.setupCell(distance: records[indexPath.row].distance, name: records[indexPath.row].name, duration: records[indexPath.row].duration)
             
             return cell
         }
@@ -68,8 +69,12 @@ extension DistanceListViewController: UITableViewDelegate, UITableViewDataSource
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        distance = records[indexPath.row].0
+        distance = records[indexPath.row].distance.rawValue
 
         performSegue(withIdentifier: unwindToMainSegue, sender: self)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
     }
 }
